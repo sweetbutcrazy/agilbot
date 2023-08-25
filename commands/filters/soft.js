6e237827befe3a78a40f32fcadc406d9a30aef97
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2023-08-25 19:53:50 +0700

    Update soft.js

--- commands/filters/soft.js
+++ commands/filters/soft.js
@@ -1,6 +1,6 @@
 const delay = require('delay');
 const chalk = require('chalk');
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const { soft } = require('../../config/filter');
 
 module.exports = { 
@@ -22,7 +22,7 @@ module.exports = {
 
         await player.setFilter('filters', soft);
 
-        const softed = new MessageEmbed()
+        const softed = new EmbedBuilder()
             .setAuthor({ name: "Turned on: Soft", iconURL: 'https://cdn.discordapp.com/emojis/758423098885275748.gif'})
             .setColor('#000001');
 
