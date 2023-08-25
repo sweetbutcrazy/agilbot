7e0939a51b26520bd50d87013194864a3059e11f
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2023-08-25 19:21:55 +0700

    Update replay.js

--- commands/music/replay.js
+++ commands/music/replay.js
@@ -1,5 +1,5 @@
 const chalk = require('chalk');
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = { 
     config: {
@@ -19,7 +19,7 @@ module.exports = {
 
         await player.seek(0);
 
-        const embed = new MessageEmbed()
+        const embed = new EmbedBuilder()
             .setDescription("\`⏮\` | **Song has been:** `Replay`")
             .setColor('#000001');
 
